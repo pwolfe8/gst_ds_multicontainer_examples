6bfe38e2065165c8cbcee175a8020b65326f2904
Philip Wolfe <pwolfe854@gmail.com>
2022-02-24 15:07:35 -0500

    some stuff'

--- examples/bash/rtspsrc_displaylocal.sh
+++ examples/bash/rtspsrc_displaylocal.sh
@@ -5,7 +5,7 @@
 
 RTSPURL1=rtsp://172.22.0.4:8554/ds-test
 RTSPURL2=rtsp://172.22.0.2:8555/ds-test2
-RTSPURL3=rtsp://admin:reolink@192.168.0.3//h264Preview_01_main
+RTSPURL3=rtsp://admin:reolink@192.168.0.11//h264Preview_01_main
 
 
 # gst-launch-1.0 -e \
@@ -22,8 +22,14 @@ DEBUG_TEMP_DIR=./debug_graphs_record
 rm -r $DEBUG_TEMP_DIR
 mkdir -p $DEBUG_TEMP_DIR
 
+DISPLAY_LOCAL="nvegltransform ! nveglglessink"
+
+
+# GST_DEBUG=1 GST_DEBUG_DUMP_DOT_DIR=$DEBUG_TEMP_DIR gst-launch-1.0 -e \
+#   rtspsrc location=$RTSPURL3 ! rtph264depay ! h264parse ! matroskamux ! filesink location=cam3_test.mkv 
 GST_DEBUG=1 GST_DEBUG_DUMP_DOT_DIR=$DEBUG_TEMP_DIR gst-launch-1.0 -e \
-  rtspsrc location=$RTSPURL3 ! rtph264depay ! h264parse ! matroskamux ! filesink location=cam3_test.mkv 
+    rtspsrc location=$RTSPURL3 ! rtph264depay ! h264parse ! videoconvert ! nveglglessink
+#   rtspsrc location=$RTSPURL3 ! rtph264depay ! h264parse ! matroskamux ! filesink location=cam3_test.mkv 
   
   
 # post pipeline dot file generation if enabled
@@ -32,3 +38,19 @@ if [ $ENABLE_DEBUG_GRAPH == true ]; then
 else 
   rm -r $DEBUG_TEMP_DIR
 fi
+
+
+# echo ""
+# echo "________________________________________"
+# # echo "displaying with input caps: "
+# # echo "    $INPUT_CAPS"
+# echo ""
+# echo hit ctrl+c to stop
+# echo "________________________________________"
+# echo ""
+
+# # gstreamer launch command
+# gst-launch-1.0 -e \
+# 	nvarguscamerasrc 
+# 	! $INPUT_CAPS_2464p \
+#   ! $DISPLAY_LOCAL
